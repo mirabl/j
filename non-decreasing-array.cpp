c1918219de49cb588c3b6de5efea780f3d4484b7
a <a@a.com>
2018-01-19 23:26:55 +0100

    .

---
+++ non-decreasing-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                if (i != 0 && nums[i + 1] < nums[i - 1]) {
+                    nums[i + 1] = nums[i];
+                } else {
+                    nums[i] = nums[i + 1];
+                }
+                for (int j = i + 1; j < nums.size() - 1; j++) {
+                    if (nums[j] > nums[j + 1]) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return true;
+    }
+};
