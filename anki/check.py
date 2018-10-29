73901b15c2197b7f96ea34ddcac15632b0f38a68
a <a@a>
2018-10-29 13:39:31 +0100

    .

--- anki/check.py
+++ anki/check.py
@@ -5,4 +5,4 @@ for f in sys.argv[1:]:
     for l in open(f):
         t = l.strip().split('\t')
         if len(t) != 2:
-            print('Error: ', t)
+            print(f if len(sys.argv[1:]) > 1 else '' + 'Error: ', t)
