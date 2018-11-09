efd0c6ace253bb3bfdc81ce671d02eb971b403e6
a <a@a>
2018-11-09 09:18:33 +0100

    .

--- search-in-rotated-array.cpp
+++ search-in-rotated-array.cpp
@@ -47,3 +47,50 @@ public:
 
 // https://discuss.leetcode.com/topic/34491/clever-idea-making-it-simple/2
 
+class Solution {
+public:
+    int findMax(vector<int>& nums) {
+        int lo = 0;
+        int hi = nums.size() - 1;
+        int n = nums.size();
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (mid == n - 1 || nums[mid + 1] < nums[mid]) {
+                return mid;
+            }
+            if (nums[mid] >= nums[0]) {
+                lo = mid + 1;
+            } else if (nums[mid] < nums[0]) {
+                hi = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+    int bs(vector<int>& nums, int target, int lo, int hi) {
+        if (hi <= lo) {
+            return nums[hi] == target ? hi : -1;
+        }
+        int mid = lo + (hi - lo) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return bs(nums, target, mid + 1, hi);
+        } else {
+            return bs(nums, target, lo, mid - 1);
+        }
+        
+    }
+    
+    int search(vector<int>& nums, int target) {
+        if (nums.empty()) {
+            return -1;
+        }
+        int iMax = findMax(nums);
+        if (target >= nums[0]) {
+            return bs(nums, target, 0, iMax);
+        } else {
+            return bs(nums, target, iMax + 1, nums.size() - 1);
+        }
+    }
+};
