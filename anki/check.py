adb56d0162d1c6c9098ad29d2a90c1ae7c99e176
a <a@a>
2018-11-13 17:38:09 +0100

    .

--- anki/check.py
+++ anki/check.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 import sys
+import glob
 
-for f in sys.argv[1:]:
+if len(sys.argv) == 1:
+    filenames = glob.glob('./[0-9].*')
+else:
+    filenames = sys.argv[1:]
+
+for f in filenames:
     for l in open(f):
         t = l.strip().split('\t')
         if len(t) != 2:
