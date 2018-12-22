4531c1869f4c72a9c608ddc7bd02a9a1caf51bc9
a <a@a>
2018-12-22 10:14:29 +0100

    .

--- linked-list-cycle-ii.cpp
+++ linked-list-cycle-ii.cpp
@@ -116,3 +116,37 @@ public:
         return NULL;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *p = head, *q = head;
+        while (p) {
+            p = p->next ? p->next->next : NULL;
+            q = q->next;
+            if (p == q) {
+                break;
+            }
+        }
+        
+        if (!p) {
+            return NULL;
+        } 
+        
+        q = head;
+        while (p != q) {
+            p = p->next;
+            q = q->next;
+        }
+        
+        return p;
+    }
+};
