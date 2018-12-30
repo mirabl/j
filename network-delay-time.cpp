0f8326e1f3ec1bb826714bf45fb9b7ff601f89d3
a <a@a>
2018-12-30 08:48:06 +0100

    .

--- network-delay-time.cpp
+++ network-delay-time.cpp
@@ -104,3 +104,78 @@ public:
     }
 };
 
+
+class Solution {
+public:
+    int infty = 1e8;
+    vector<int> dist;
+    vector<bool> done;
+    vector<vector<pair<int, int>>> G;
+    
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        dist = vector<int>(N, infty);
+        done = vector<bool>(N, false);
+        G = vector<vector<pair<int, int>>>(N);
+        for (auto e: times) {
+            G[e[0] - 1].push_back({e[1] - 1, e[2]});
+        }
+        K--;
+        dist[K] = 0;
+        done[K] = true;
+        for (auto e: G[K]) {
+            dist[e.first] = e.second;
+        }
+        
+        for (int j = 0; j < N - 1; j++) {
+            int nextNode = -1;
+            for (int i = 0; i < N; i++) {
+                if (!done[i] && (nextNode == -1 || dist[i] < dist[nextNode])) {
+                    nextNode = i;
+                }
+            }
+            if (nextNode == -1) {
+                break;
+            }
+            for (auto e: G[nextNode]) {
+                int v = e.first;
+                dist[v] = min(dist[v], dist[nextNode] + e.second);
+            } 
+            done[nextNode] = true;
+        }
+        
+        int mx = 0;
+        for (int i = 0; i < N; i++) {
+            mx = max(mx, dist[i]);
+        }
+        return mx < infty ? mx : -1;
+    }
+};
+
+class Solution {
+public:
+    int infty = 1e6;
+    vector<int> dist;
+    vector<vector<pair<int, int>>> G;
+    void dfs(int src, int w) {
+        if (dist[src] <= w) {
+            return;
+        }
+        dist[src] = w;
+        for (auto n: G[src]) {
+            dfs(n.first, w + n.second);
+        }
+    }
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        dist = vector<int>(N, infty);
+        G = vector<vector<pair<int, int>>>(N);
+        for (auto e: times) {
+            G[e[0] - 1].push_back({e[1] - 1, e[2]});
+        }
+        dfs(K - 1, 0);
+        int mx = dist[0];
+        for (int i = 1; i < N; i++) {
+            mx = max(mx, dist[i]);
+        }
+        return mx < infty ? mx : -1;
+    }
+};
