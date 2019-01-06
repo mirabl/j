abcdf085c9f4d35dcdfae66d692dada0272c75dd
a <a@a>
2019-01-06 09:50:40 +0100

    .

--- search-in-rotated-array.cpp
+++ search-in-rotated-array.cpp
@@ -191,3 +191,48 @@ public:
     }
 };
 
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.empty()) {
+            return -1;
+        }
+        int N = nums.size();
+        int lo = 0;
+        int hi = N - 1;
+        while (lo != hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] > nums[N - 1]) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        
+        if (nums[0] <= nums[N - 1]) {
+            lo = 0;
+            hi = N - 1;
+        } else if (target >= nums[0]) {
+            lo = 0;
+            hi = max(0, hi - 1);
+        } else {
+            lo = hi;
+            hi = N - 1;
+        }
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
