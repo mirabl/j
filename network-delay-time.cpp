e6d7d2b05d837919b8db87691de6fa0a712e840a
a <a@a>
2019-01-06 10:33:04 +0100

    .

--- network-delay-time.cpp
+++ network-delay-time.cpp
@@ -301,3 +301,39 @@ public:
         return res < infty ? res : -1;
     }
 };
+
+// DFS
+class Solution {
+public:
+    vector<int> dist;
+    int infty = 1e8;
+    vector<vector<pair<int, int>>> G;
+    
+    void dfs(int i, int d) {
+        if (d >= dist[i]) {
+            return;
+        }
+        dist[i] = d;
+        for (auto p: G[i]) {
+            dfs(p.first, d + p.second);
+        }
+    }
+    
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        dist = vector<int>(N, infty);
+        G = vector<vector<pair<int, int>>>(N);
+        
+        for (auto time: times) {
+            G[time[0] - 1].push_back({time[1] - 1, time[2]});
+        }
+        K--;
+        
+        dfs(K, 0);
+        
+        int res = 0;
+        for (int i = 0; i < N; i++) {
+            res = max(res, dist[i]);
+        }
+        return res < infty ? res : -1;
+    }
+};
