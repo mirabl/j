df3afd043ee8945e897928bd2a07157e8b6968e0
a <a@a>
2019-02-28 13:41:59 +0100

    .

---
+++ insert-into-a-binary-search-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) {
+            return new TreeNode(val);
+        }
+        
+        if (val < root->val) {
+            if (!root->left) {
+                root->left = new TreeNode(val);
+            } else {
+                insertIntoBST(root->left, val);
+            }
+        } else {
+            if (!root->right) {
+                root->right = new TreeNode(val);
+            } else {
+                insertIntoBST(root->right, val);
+            }
+        }
+        
+        return root;
+    }
+};
