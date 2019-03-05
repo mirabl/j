98e4939b4298de4d67817c424394e2788f7e0e69
a <a@a>
2019-03-05 07:09:23 +0100

    .

---
+++ rlc.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# ./rlc.sh level
+# level: 2 med, 3 hard
+
+cat ../lc.json |jq ".stat_status_pairs | .[] | select(.status == null and .paid_only == false and .difficulty.level == $1) | .stat.question__title_slug" | tr -d '"'| sed 's|^|https://leetcode.com/problems/|' | sort -R | head -4 | xargs open
