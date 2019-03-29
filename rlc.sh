0f2c5f18fb5093888cda2ba5a605fba50de3da68
a <a@a>
2019-03-29 16:04:44 +0100

    .

--- rlc.sh
+++ rlc.sh
@@ -2,4 +2,4 @@
 # ./rlc.sh level
 # level: 2 med, 3 hard
 
-cat ../lc.json |jq ".stat_status_pairs | .[] | select(.status == null and .paid_only == false and .difficulty.level == $1) | .stat.question__title_slug" | tr -d '"'| sed 's|^|https://leetcode.com/problems/|' | sort -R | head -4 | xargs open
+cat ../lc.json |jq ".stat_status_pairs | .[] | select(.status == null and .paid_only == false and .difficulty.level == $1) | .stat.question__title_slug" | tr -d '"'| sed 's|^|https://leetcode.com/problems/|' | sort -R | head -10 | xargs open
