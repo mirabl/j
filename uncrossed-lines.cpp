5cec32255f6fcf2ac2418116d06060e08d05b526
a <a@a>
2019-05-22 17:48:08 +0200

    .

--- uncrossed-lines.cpp
+++ uncrossed-lines.cpp
@@ -29,3 +29,23 @@ public:
         return r(A, 0, B, 0);
     }
 };
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        int m = B.size();
+        
+        vector<vector<int>> T(n + 1, vector<int>(m + 1, 0));
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (A[i] == B[j]) {
+                    T[i][j] = 1 + T[i + 1][j + 1];
+                } else {
+                    T[i][j] = max(T[i + 1][j], T[i][j + 1]);
+                }
+            }
+        }
+        return T[0][0];
+        
+    }
+};
