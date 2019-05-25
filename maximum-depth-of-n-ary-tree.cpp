8dd4b8e0aa73e10486cf53ec59fd78deb301c66a
a <a@a>
2019-05-25 08:52:09 +0200

    .

--- maximum-depth-of-n-ary-tree.cpp
+++ maximum-depth-of-n-ary-tree.cpp
@@ -35,3 +35,31 @@ public:
         return d;
     }
 };
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if (!root) {
+            return 0;
+        }
+        int b = 1;
+        for (auto c: root->children) {
+            b = max(b, 1 + maxDepth(c));
+        }
+        return b;
+    }
+};
