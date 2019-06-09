1c917982606b8e3f025a718581aa77acf04e0681
a <a@a>
2019-06-09 17:36:38 +0200

    .

--- vertical-order-traversal-of-a-binary-tree.cpp
+++ vertical-order-traversal-of-a-binary-tree.cpp
@@ -42,3 +42,47 @@ public:
         return res;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    map<int, vector<pair<int, int>>> byX;
+    
+    void rec(TreeNode *root, int x, int y) {
+        if (!root) {
+            return;
+        }
+        
+        byX[x].push_back({ y, root->val });
+        rec(root->left, x - 1, y + 1);
+        rec(root->right, x + 1, y + 1);
+    }
+    
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> res;
+        rec(root, 0, 0);
+        
+        for (auto kvp: byX) {
+            auto l = kvp.second;
+            if (l.empty()) {
+                continue;
+            }
+            sort(l.begin(), l.end());
+            vector<int> ll;
+            for (auto p: l) {
+                ll.push_back(p.second);
+            }
+            res.push_back(ll);
+            
+        }
+        return res;
+    }
+};
