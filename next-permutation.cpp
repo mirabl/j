710668bf24b24da008cc53ac469084b387eb42d7
a <a@a>
2019-07-04 19:51:00 +0200

    .

--- next-permutation.cpp
+++ next-permutation.cpp
@@ -77,3 +77,29 @@ public:
         reverse(nums.begin() + i + 1, nums.end());
     }
 };
+
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int j = n - 2;
+        while (j >= 0 && nums[j] >= nums[j + 1]) {
+            j--;
+        }
+        
+        if (j < 0) {
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        for (int i = n - 1; i >= j; i--) {
+            if (nums[i] > nums[j]) {
+                swap(nums[i], nums[j]);
+                reverse(nums.begin() + j + 1, nums.end());
+                return;
+            }
+        }
+        
+    }
+};
