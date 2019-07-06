319f035e96252fe54a3ae20c008affb81366777e
a <a@a>
2019-07-06 11:20:04 +0200

    .

--- implement-stack-using-queues.cpp
+++ implement-stack-using-queues.cpp
@@ -41,3 +41,53 @@ public:
  * int param_3 = obj.top();
  * bool param_4 = obj.empty();
  */
+
+struct LLNode {
+    int val;
+    LLNode* next;
+    LLNode(int v): val(v), next(NULL) {}
+    LLNode(): LLNode(0) {}
+};
+
+class MyStack {
+public:
+    LLNode* head;
+    
+    /** Initialize your data structure here. */
+    MyStack() {
+        head = NULL;
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        auto n = new LLNode(x);
+        n->next = head;
+        head = n;
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int v = head->val;
+        head = head->next;
+        return v;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return head->val;
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return head == NULL;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
