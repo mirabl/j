667221e47b2147c4f27c60d74271ec7e555d33c7
a <>
2019-11-09 19:14:22 +0100

    .

--- implement-queue-using-stacks.cpp
+++ implement-queue-using-stacks.cpp
@@ -178,3 +178,58 @@ public:
  * int param_3 = obj->peek();
  * bool param_4 = obj->empty();
  */
+struct LLNode {
+    int val;
+    LLNode *next;
+    LLNode(int v): val(v), next(NULL) { }
+};
+
+class MyQueue {
+public:
+    LLNode *t = NULL;
+    LLNode *h = NULL;
+    /** Initialize your data structure here. */
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        LLNode *n = new LLNode(x);
+        if (h == NULL) {
+            h = n;
+            t = n;
+        } else {
+            t->next = n;
+            t = n;
+        }
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int v = peek();
+        LLNode *x = h;
+        h = h->next;
+        delete(x);
+        return v;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        return h->val;
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return h == NULL;
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
