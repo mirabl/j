6fea3fc3d4235d507aab17ee421b494fa094e9e9
a <>
2019-11-30 18:59:03 +0100

    .

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,7 @@
 alias ga='git add'
 alias gc='git commit --allow-empty-message -m "."'
 alias gd='git diff'
-alias gg='ga .gitignore KATA-LOG LESSON_LEARNED TODO aliases.sh *cpp anki/*tsv *tsv; gc; gp'
+alias gg='ga .gitignore KATA-LOG LESSON_LEARNED TODO aliases.sh *cpp anki/*tsv *tsv *.java; gc; gp'
 alias ggg='pushd .; cdanki; cd ..; gg; popd'
 alias gp='git push -u origin master'
 alias gs='git status'
