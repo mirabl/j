3e29120a06bce0e3ae03761ccab3329c5bfb5328
a <>
2019-12-01 09:54:35 +0100

    .

---
+++ linked-list-cycle-ii.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode p = head;
+        ListNode q = head;
+        while (p != null && p.next != null) {
+            p = p.next.next;
+            q = q.next;
+            
+            if (p == q) {
+                q = head;
+                while (p != q) {
+                    p = p.next;
+                    q = q.next;
+                }
+                return p;
+            }   
+        }
+        return null;
+    }
+}
